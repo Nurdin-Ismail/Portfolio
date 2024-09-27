460591e23158cab20cb978b5d9d7fcccb3a485b8
Nurdin Ismail <shiilnurdin@gmail.com>
2024-09-27 18:42:16 +0300

    converted all the tailwind css to raw css

--- client/src/components/ProjectMedia.jsx
+++ client/src/components/ProjectMedia.jsx
@@ -59,14 +59,14 @@ export default function ProjectMedia({ current, setcurrent }) {
 
         if (current == 'decora' && slides) {
 
-            return <div className='grid place-content-center my-8 py-2 mx-40 border-[#d1c0c0] border-[2px] rounded-xl overflow-hidden'>
+            return <div className='decora-div '>
                 <div
-                    className='flex best gap-5 ml-4'
-                    style={{ transform: `translateX(-${slide * 1520}px)` }} // Calculating the translate
+                    className='best'
+                    style={{ transform: `translateX(-${slide * 100}%)` }} // Calculating the translate
                 >
 
                     {slides.map((item, index) => (
-                        <video key={index} width="1500px" autoPlay loop muted playsInline>
+                        <video key={index} className='decora-vid' autoPlay loop muted playsInline>
                             <source src={item} type="video/mp4" />
                         </video>
                     ))}
@@ -79,29 +79,29 @@ export default function ProjectMedia({ current, setcurrent }) {
 
 
         } else if (current == 'restomate' && slides) {
-            return <div className=' place-content-center my-8 py-2 mx-40 border-[#d1c0c0] border-[2px] overflow-hidden rounded-xl '>
+            return <div className=' decora-div '>
                 <div
-                    className='grid grid-flow-col  best  gap-5 ml-4 '
-                    style={{ transform: `translateX(-${slide * 1520}px)` }} // Calculating the translate
+                    className='best'
+                    style={{ transform: `translateX(-${slide * 100}%)` }} // Calculating the translate
                 >
                     {slides.map((item, index) => (
                         <img
                             src={item}
                             alt=""
                             key={index}
-                            className='  min-w-[1500px] h-[70vh]'
+                            className='  decora-vid'
                         />
                     ))}
                 </div>
             </div>
 
         } else if (current == 'odoo') {
-            return <div className=' place-content-center my-8 py-2 mx-40 border-[#d1c0c0] border-[2px] overflow-hidden rounded-xl '>
+            return <div className=' decora-div'>
                 <div
-                    className='grid grid-flow-col  best  gap-5 ml-4 '
+                    className=''
                 >
 
-                    <video width="1500px" autoPlay loop muted playsInline>
+                    <video className='decora-vid' autoPlay loop muted playsInline>
                         <source src={odoo} type="video/mp4" />
                     </video>
 
@@ -116,28 +116,37 @@ export default function ProjectMedia({ current, setcurrent }) {
     return (
         <div>
             <div
-                className='modal fixed h-[100vh] top-[0vh] flex justify-center items-center left-[0vw] w-[100vw] modalo'
+                className='modal-bg '
                 onClick={() => setcurrent(false)}
             ></div>
 
-            <div className='modal fixed text-black text-xl h-[90vh] top-[5vh] left-[5vw] w-[90vw] bg-white rounded-lg '>
+            <div className='modal  '>
 
 
 
                 {handleContent()}
 
 
-                <div className='title grid grid-cols-3 h-[10vh]'>
+                <div className='content-name '>
+                    {/* arrow left */}
                     <div className={current == 'odoo' ? ' hidden' : 'center-end '}>
-                        <div className={slide == 0 ? ' z-20 arrow1 rounded-full bg-violet-200 h-[40px] w-[40px] opacity-50 ' : ' z-20 arrow1 rounded-full bg-violet-200 h-[40px] w-[40px] center-end '} onClick={() => handleslides('left')}><img className=" cursor-pointer rotate-180 h-[25px] mx-[6px] my-[6px]  " src={forward} alt="" /></div>
+                        <div 
+                        className={slide == 0 ? ' arrow1  opacity-50 ' 
+                        :
+                         ' arrow1 '} 
+                         onClick={() => handleslides('left')}>
+                            <img className="  " src={forward} alt="" />
+                            </div>
 
                     </div>
-                    <div className='grid place-self-center overflow-hidden'>
 
-                        <div className='flex best  w-[616px]'
+                    {/* names */}
+                    <div className='names-div'>
+
+                        <div className='names-container best'
                             style={{ transform: `translateX(-${slide * 600}px)` }}>
                             {titles ? titles.map((item, index) => {
-                                return <h1 key={index} className='min-w-[600px] text-center'>
+                                return <h1 key={index} className='name'>
                                     {item}
                                 </h1>
 
@@ -145,8 +154,18 @@ export default function ProjectMedia({ current, setcurrent }) {
                         </div>
 
                     </div>
-                    <div className={current == 'odoo' ? 'hidden' : 'center-start'}>
-                        <div className={slides.length - 1 != slide ? ' z-30 arrow2 rounded-full bg-violet-200 h-[40px] w-[40px] center-start ' : ' z-30 arrow2 rounded-full bg-violet-200 h-[40px] w-[40px] center-start opacity-50 '} onClick={() => handleslides('right')}><img className=" cursor-pointer h-[25px] mx-[8px] my-[7.5px]" src={forward} alt="" /></div>
+                    
+                    {/* arrow right */}
+                    <div 
+                    className={current == 'odoo' ? 'hidden' : 'center-start'}>
+                        <div
+                        className={slides.length - 1 != slide ? 
+                        ' arrow2   ' 
+                        : 
+                        ' arrow2 opacity-50 '} 
+                        onClick={() => handleslides('right')}>
+                            <img className="" src={forward} alt="" />
+                            </div>
 
                     </div>
                 </div>
